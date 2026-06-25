9eb2e64687117249a8ffd3d1ebf44d378ad65e2f
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-06-25 13:34:28 +0300

    impl deanon command

--- kuznetsov.petr/U2/commands.cpp
+++ kuznetsov.petr/U2/commands.cpp
@@ -17,9 +17,19 @@ void kuznetsov::detail::sort(darray< size_t >& arr)
   }
 }
 
+size_t kuznetsov::detail::findPersonIdx(darray< Person >& pers, size_t id)
+{
+  for (size_t i = 0; i < pers.size; ++i) {
+    if (pers.data[i].id == id) {
+      return i;
+    }
+  }
+  return pers.cap;
+}
+
 void kuznetsov::anons(std::istream&, std::ostream& out, darray< Meeting >& mts, darray< Person >& pers)
 {
-  darray< size_t > ids = makeDarray(8);
+  darray< size_t > ids = makeDarray< size_t >(8);
   for (size_t i = 0; i < pers.size; ++i) {
     if (pers.data[i].info == "") {
       pushBackDarray(ids, pers.data[i].id);
@@ -28,6 +38,38 @@ void kuznetsov::anons(std::istream&, std::ostream& out, darray< Meeting >& mts, 
   detail::sort(ids);
   for (size_t i = 0; i < ids.size; ++i) {
     out << ids.data[i] << '\n';
+  }
+}
+
+void kuznetsov::deanon(std::istream& in, std::ostream&, darray< Meeting >& mts, darray< Person >& prs)
+{
+  size_t anonId, id;
+  in >> anonId >> id;
+  if (in.fail()) {
+    throw std::logic_error("Smth go bad");
+  }
+  if (prs.data[detail::findPersonIdx(prs, anonId)].info != "") {
+    throw std::logic_error("Bad anon");
+  }
+  if (prs.data[detail::findPersonIdx(prs, id)].info == "") {
+    throw std::logic_error("Bad id");
+  }
+
+  for (size_t i = 0; i < mts.size; ++i) {
+    if (mts.data[i].id1 == anonId) {
+      mts.data[i].id1 = id;
+    }
+    if (mts.data[i].id2 == anonId) {
+      mts.data[i].id2 = id;
+    }
+  }
+  size_t i = 0;
+  while (i < mts.size) {
+    if (mts.data[i].id1 == mts.data[i].id2) {
+      removeDarray(mts, i);
+      i -= 2;
+    }
+    i += 1;
   }
 }
 
